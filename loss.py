cd2505526e6ece4f365ed26316d18cca633b1306
Andriy <freetown113@gmail.com>
2023-05-25 15:40:23 +0200

    fixed errors with in arguments passing and model creation

--- loss.py
+++ loss.py
@@ -8,14 +8,17 @@ import numpy as np
 from models import Encoder, Decoder, Quantizer, VQ_VAE
 
 
-def create_fwd_fn(model, **kwargs):
+def create_fwd_fn(model, *args, **kwargs):
     def fwd_pass(input):
-        return model(**kwargs)(input)
+        return model(*args, **kwargs)(input)
     return hk.without_apply_rng(hk.transform(fwd_pass))
 
 
-def make_loss(input):
-    enc = Encoder()
-    dec = Decoder()
-    quant = Quantizer()
-    model = VQ_VAE(enc, dec, quant, )
+def make_loss_fn(input, var, kwargs):
+    args = {kwargs[i].name: kwargs[i].value for i in dir(kwargs)}
+    enc = Encoder
+    dec = Decoder
+    quant = Quantizer
+    model = create_fwd_fn(VQ_VAE, enc, dec, quant, var, **args)
+    return model
+    
