490e44756384516042fc54c5c9a646e06ccccb39
Andriy <freetown113@gmail.com>
2023-05-25 15:41:24 +0200

    fixed shapes issues, arguments passing process, model initialization

--- models.py
+++ models.py
@@ -15,47 +15,47 @@ from cifar import get_loader
 
 
 class Encoder(hk.Module):
-    def __init__(self, out_ch, hiddens=(8, 16, 32, 32, 64), name=None)
-        super().__init(name=name)
-        self.out = out_ch
-        self.hid = hiddens
+    def __init__(self, enc_hid=(8, 16, 32, 32, 64), name=None):
+        super().__init__(name=name)
+        self.hid = tuple([int(i) for i in enc_hid])
 
     def __call__(self, x):
-        for id, out in enumerate(hidden, start=1):
-            x = hk.Conv2D(out, [4, 4], stride=1 if id%2 else 2,
+        for id, out in enumerate(self.hid, start=1):
+            x = hk.Conv2D(out, (4, 4), stride=1 if id%2 else 2,
                           padding='SAME', name=f'enc_{id}')(x)
             x = jax.nn.leaky_relu(x)
 
-        x = hk.Conv2D(out, [1, 1], stride=1)(x)
+        x = hk.Conv2D(out, (1, 1), stride=1)(x)
         return x
 
 
 class Decoder(hk.Module):
-    def __init__(self, in_ch, hiddens=(32, 32, 16, 8, 3), name=None):
+    def __init__(self, dec_hid=(32, 32, 16, 8, 3), name=None):
        super().__init__(name=name)
-       self.input = in_ch
-       self.hid = hiddens
+       self.hid = tuple([int(i) for i in dec_hid])
 
     def __call__(self, x):
        for i, out in enumerate(self.hid, start=1):
-           x = hk.Conv2DTranspose(out, [4, 4], stride=1 if id%2 else 2,
+           x = hk.Conv2DTranspose(out, [4, 4], stride=1 if i%2 else 2,
                                   padding='SAME', name=f'dec_{i}')(x)
            x = jax.nn.leaky_relu(x)
        return x
-        
+
 
 class Quantizer(hk.Module):
-    def __init__(self, num_emb, emb_dim, commitment_coeff=0.25):
-        super().__init__()
+    def __init__(self, num_emb, emb_dim, commitment_coeff=0.25, name=None):
+        super().__init__(name=name)
         self.dim = emb_dim
         self.num = num_emb
         self.com_coef = commitment_coeff
-        self.dict = hk.get_parameter('EmbeddingDictionary', shape=(num_emb, emb_dim), 
-                                     type=np.float32, 
+        self.dict = hk.get_parameter(name='EmbeddingDictionary', 
+                                     shape=(emb_dim, num_emb), 
+                                     dtype=np.float32, 
                                      init=hk.initializers.VarianceScaling(distribution="uniform"))
 
         
     def __call__(self, x):
+        input = x
         assert x.shape[-1] == self.dim
         original_shape = x.shape
 
@@ -64,16 +64,16 @@ class Quantizer(hk.Module):
 
         indexes = jnp.argmin(dists, axis=-1)
         encodings = jax.nn.one_hot(indexes, self.num)
+        
+        quantized = jnp.matmul(encodings, jnp.transpose(self.dict, axes=(1,0)))
+        quantized = jnp.reshape(quantized, original_shape)
 
-        quantized = jnp.matmul(encodings, self.dict, transpose_b=True)
-        quantized = jnp.reshape(original_shape)
-
-        latent_loss = jnp.square(jax.lax.stop_gradient(x) - quatized)
-        commitment_loss = jnp.square(jax.lax.stop_gradient(quatized) - x)
+        latent_loss = jnp.square(jax.lax.stop_gradient(input) - quantized)
+        commitment_loss = jnp.square(jax.lax.stop_gradient(quantized) - input)
   
         loss = latent_loss + self.com_coef * commitment_loss
 
-        quantized = x + jax.lax.stop_gradient(quantized - x)
+        quantized = input + jax.lax.stop_gradient(quantized - input)
         avg_probs = jnp.mean(encodings, 0)
         perplexity = jnp.exp(-jnp.sum(avg_probs * jnp.log(avg_probs + 1e-10)))
 
@@ -84,19 +84,19 @@ class Quantizer(hk.Module):
     def calc_distances_matrix(self, x):
         similarity = jnp.matmul(x, self.dict)
 
-        distances = jnp.sum(jnp.square(x), axis=-1) + \
+        distances = jnp.sum(jnp.square(x), axis=-1, keepdims=True) + \
                     jnp.sum(jnp.square(self.dict), axis=0) - \
                     2 * similarity
 
         return distances
 
 
-class VQ_VAE(hk.Modeule):
-    def __init__(self, enc, dec, quant, variance, name=None)
-        super().__init__(self)
-        self.enc = enc
-        self.dec = dec
-        self.quant = quant
+class VQ_VAE(hk.Module):
+    def __init__(self, enc, dec, quant, variance, *args, **kwargs):
+        super().__init__(name=kwargs['name'])
+        self.enc = enc(kwargs['enc_hid'], name=kwargs['name']+'_ENC')
+        self.dec = dec(kwargs['dec_hid'], name=kwargs['name']+'_DEC')
+        self.quant = quant(kwargs['num_emb'], kwargs['emb_dim'], name=kwargs['name']+'_QNT')
         self.var = variance
 
     def __call__(self, x):
